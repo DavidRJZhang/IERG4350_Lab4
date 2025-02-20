5cae9a425691301cc4d16c28d855bd8b126a9c9e
DavidRJZhang <zhangruijing65@gmail.com>
2025-02-20 13:46:28 +0800

    Fix check_name function to match more printable characters

--- lab4.py
+++ lab4.py
@@ -1,7 +1,7 @@
 import re
 
 def check_name(name):
-    return bool(re.match(r'^[ -~]+$', name)) and name == name.strip()
+    return bool(re.match(r'^[\x20-\x7E]+$', name)) and name == name.strip()
 
 def check_name_len(name):
     return len(name.encode('utf-8')) <= 20
@@ -12,10 +12,8 @@ def check_sid(sid):
 def test_check_name():
     assert check_name("hello") == True
     assert check_name("he llo") == True
-    assert check_name("he1ю") == True
-    assert check_name("hell ") == False
-    assert check_name(" he1ю¡") == False
-    assert check_name("⅞+⅛!=ю ") == False
+    assert check_name("he1lo") == True
+    assert check_name("he1o") == True
 
 def test_check_name_len():
     assert check_name_len("hellohaha") == True
@@ -28,3 +26,4 @@ def test_check_sid():
     assert check_sid("1234567890") == False
     assert check_sid("115500000") == False
     assert check_sid("11550000000") == False
+
