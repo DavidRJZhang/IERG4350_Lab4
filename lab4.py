3a8cb75182f6413165bcec21675d6b3c4b727151
DavidRJZhang <zhangruijing65@gmail.com>
2025-02-20 12:41:14 +0800

    Add lab4.py with check functions and tests

---
+++ lab4.py
@@ -0,0 +1,30 @@
+import re
+
+def check_name(name):
+    return bool(re.match(r'^[ -~]+$', name)) and name == name.strip()
+
+def check_name_len(name):
+    return len(name.encode('utf-8')) <= 20
+
+def check_sid(sid):
+    return bool(re.match(r'^1155\d{6}$', sid))
+
+def test_check_name():
+    assert check_name("hello") == True
+    assert check_name("he llo") == True
+    assert check_name("he1ю") == True
+    assert check_name("hell ") == False
+    assert check_name(" he1ю¡") == False
+    assert check_name("⅞+⅛!=ю ") == False
+
+def test_check_name_len():
+    assert check_name_len("hellohaha") == True
+    assert check_name_len("⅞+⅛!=ю") == True
+    assert check_name_len("ю ю") == True
+    assert check_name_len("⅞+⅛!=ю!!!!") == True
+
+def test_check_sid():
+    assert check_sid("1155000000") == True
+    assert check_sid("1234567890") == False
+    assert check_sid("115500000") == False
+    assert check_sid("11550000000") == False
